0a30b29e8dfad72ea681aaa2b0875612e310f82f
benjdj6 <benjdj6@vt.edu>
2017-05-28 15:53:29 -0400

    now creating an Image object

--- solution.py
+++ solution.py
@@ -1,12 +1,26 @@
 import requests
+from PIL import Image
 
 # Written by Ben Johnston on May 28, 2017
 
 
 def getRandNum():
-    queryst = {'num': 256, 'min': 0, 'max': 256, 'col': 256, 'base': 10, 'format': 'plain', 'rnd': 'new'}
+    queryst = {
+        'num': 10000,
+        'min': 0,
+        'max': 255,
+        'col': 256,
+        'base': 10,
+        'format': 'plain',
+        'rnd': 'new'
+    }
     r = requests.get('https://www.random.org/integers', params=queryst)
     return r.text
 
 
-print getRandNum()
+def createBMP():
+    im = Image.new('RGB', (128, 128))
+    return im
+
+
+print createBMP()
